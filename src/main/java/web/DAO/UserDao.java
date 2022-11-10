e20fefe1001d92cd1db6b46e946c2054ba921a36
Sergey Pichugin <rutilus89@mail.ru>
2022-11-10 17:11:30 +0300

    Initial commit

---
+++ src/main/java/web/DAO/UserDao.java
@@ -0,0 +1,14 @@
+package web.DAO;
+
+import web.model.User;
+import java.util.List;
+
+public interface UserDao {
+
+    List<User> getAllUsers();
+    void save(User user);
+    User show(int id);
+    void update(int id, User updateUser);
+    void delete(int id);
+    User isExistById(User user);
+}
---
+++ src/main/java/web/Service/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package web.Service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import web.DAO.UserDao;
+import web.model.User;
+import java.util.List;
+
+@Component
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+
+    private final UserDao userDao;
+
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public void save(User user) {
+        userDao.save(user);
+    }
+
+    @Override
+    public User show(int id) {
+        return userDao.show(id);
+    }
+
+    @Override
+    @Transactional
+    public void update(int id, User updateUser) {
+        userDao.update(id, updateUser);
+    }
+
+    @Override
+    @Transactional
+    public void delete(int id) {
+        userDao.delete(id);
+    }
+
+    @Override
+    @Transactional
+    public User isExistById(User user) {
+        return userDao.isExistById(user);
+    }
+}
